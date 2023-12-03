646b4da921f632f57630c84aefc19388b9f980ba
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2023-12-03 22:05:03 +0330

    Ex1: Parsing class added

---
+++ Exercise-1/src/main/java/org/packing/Parsing.java
@@ -0,0 +1,21 @@
+package org.packing;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Parsing {
+
+    String input;
+
+
+    public Commodity parseOne(String input){
+        short inputLength = (short) input.length();
+        String inputWithoutParentheses = input.substring(1,inputLength-1);
+        List<String> separatedList = Arrays.asList(inputWithoutParentheses.split(","));
+        byte id = Byte.parseByte(separatedList.get(0));
+        byte weight = Byte.parseByte(separatedList.get(1));
+        String valueWithoutDollarSign = (separatedList.get(2)).substring(1);
+        byte value = Byte.parseByte(valueWithoutDollarSign);
+        return new Commodity (id, weight, value);
+    }
+}
