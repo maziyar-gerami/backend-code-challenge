43a8b6f9b52ce06cb6358051d19c5e3f901eaba9
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2023-12-04 20:42:15 +0330

    parsing refactored

--- src/main/java/com/maziyar/services/packing/Parsing.java
+++ src/main/java/com/maziyar/services/packing/Parsing.java
@@ -1,37 +1,26 @@
 package com.maziyar.services.packing;
 
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Parsing {
-    public Product parseOneParentheses(String commoditySpecs){
-        short inputLength = (short) commoditySpecs.length();
-        String inputWithoutParentheses = commoditySpecs.substring(1,inputLength-1);
-        List<String> separatedList = Arrays.asList(inputWithoutParentheses.split(","));
-        int id = Integer.valueOf(separatedList.get(0));
-        float weight = Float.valueOf(separatedList.get(1));
-        String valueWithoutDollarSign = (separatedList.get(2)).substring(1);
-        int value = Integer.valueOf(valueWithoutDollarSign);
-        return new Product (id, weight, value);
+
+    public Product parseOneParentheses(String commoditySpecs) {
+        String[] parts = commoditySpecs.substring(1, commoditySpecs.length() - 1).split(",");
+        int id = Integer.parseInt(parts[0]);
+        float weight = Float.parseFloat(parts[1]);
+        int value = Integer.parseInt(parts[2].substring(1));
+        return new Product(id, weight, value);
     }
 
     public String[] splitByParentheses(String input) {
-        String inputWithoutSpacing = input.replaceAll("\\s", "");
-        return inputWithoutSpacing.split("(?<=\\))|(?=\\()");
+        return input.replaceAll("\\s", "").split("(?<=\\))|(?=\\()");
     }
 
-public  List<Product> commoditiesToArrays(String input){
-        String [] split = splitByParentheses(input);
-
-        List<Product> allCommodities = new LinkedList<>();
-        for (String s:split)
-            allCommodities.add(parseOneParentheses(s));
-        
-            return allCommodities;
+    public List<Product> parseAllCommodities(String input) {
+        return Arrays.stream(splitByParentheses(input))
+                .map(this::parseOneParentheses)
+                .collect(Collectors.toList());
+    }
 }
-
-
-}
-
-
